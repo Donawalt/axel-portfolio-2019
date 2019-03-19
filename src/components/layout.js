cd73d3da584efbcc04c861211b939ff1eac98b37
Donaël Walt <w.dodo.a@hotmail.fr>
2019-03-19 23:06:57 +0100

    sensitive case 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Helmet from 'react-Helmet'
+import Header from "./header"
+import Footer from "./footer"
+import Helmet from 'react-helmet'
 
 import '../style/main.scss'
 
