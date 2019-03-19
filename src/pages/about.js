352ea568e5129170c4645c903ce47a6e343931b4
Donaël Walt <w.dodo.a@hotmail.fr>
2019-03-19 21:47:28 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,24 @@
 import React from "react"
 import Layout from '../components/Layout'
+import profile from '../images/profileAxel.jpg'
 
 class AboutPage extends React.Component {
   render(){
     return (
       <Layout type="aboutPage">
         <main>
-          <section>
+          <section className="left">
             <div className="content-text">
               <p>👋 I'm <strong>Axel Dos Santos</strong></p>
               <p>lorem ipsum dolor sit amet consectur , blablablablabala bala balal bla bala blablabn...</p>
             </div>
             <div className="content-info"></div>
           </section>
-          <section>
-            
+          <section className="right">
+            <div className="profile">
+              <img src={profile} alt="photodeprofile"/>
+            </div>
+            <div className="background"></div>
           </section>
         </main>
       </Layout>
